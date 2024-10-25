851c618406c82632d64e54a673a5b6608f0e1200
ilorez <zobirnajdaoui@gmail.com>
2024-10-25 10:49:07 +0100

    hello

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,17 +6,17 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 16:18:51 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/10/24 16:51:04 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/10/25 10:12:19 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stdlib.h"
-#include "stdio.h"
-#include "string.h"
+//#include "stdlib.h"
+//#include "stdio.h"
+//#include "string.h"
 
-size_t	ft_strlcpy(char *dst, const char * src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t len;
+	size_t	len;
 
 	len = 0;
 	if (dstsize)
@@ -35,18 +35,19 @@ size_t	ft_strlcpy(char *dst, const char * src, size_t dstsize)
 	return (len);
 }
 
-
-int main(int ac, char **av)
+/*int main(int ac, char **av)
 {
-	size_t dstlen = atoi(av[2]);
-	
-	char str2[dstlen];
-	char str3[dstlen];
-	size_t len = ft_strlcpy(str2, av[1], dstlen);
-	size_t len_original = strlcpy(str3, av[1], dstlen);
+	size_t	dstlen;
+	char	str2[dstlen];
+	char	str3[dstlen];
+	size_t	len;
+	size_t	len_original;
 
-	
-	printf ("str: %s,\nstrlcpy:%s", av[1], str2);	
-	printf ("original ------\nstr: %s,\nstrlcpy:%s", av[1], str3 );	
-	return ac;
+	dstlen = atoi(av[2]);
+	len = ft_strlcpy(str2, av[1], dstlen);
+	len_original = strlcpy(str3, av[1], dstlen);
+	printf ("str: %s,\nstrlcpy:%s", av[1], str2);
+	printf ("original ------\nstr: %s,\nstrlcpy:%s", av[1], str3 );
+	return (ac);
 }
+*/
