2ce994192bddac4898d3c5f63606fd675db599c1
ilorez <zobirnajdaoui@gmail.com>
2024-10-26 09:37:57 +0100

    more test in ft_memcmp with original funtion

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 08:58:14 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/10/26 09:32:52 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/10/26 09:37:24 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,9 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	str1 = av[1];
 	char *str2;
 	str2 = av[2];
-	int diff = (int)ft_memcmp((const void *)str1,
-			(const void *)str2,atoi(av[3]));
+	int diff = (int)ft_memcmp((const void *)str1,(const void *)str2,atoi(av[3]));
+	int diff2 = (int)memcmp((const void *)str1,(const void *)str2,atoi(av[3]));
 	printf("str1:%s\nstr2:%s\ndeferent byte:%d\n", str1, str2, diff);
+	printf("oringinal deferent byte:%d\n", diff2);
 	return (ac);
 }*/
