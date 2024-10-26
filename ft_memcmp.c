e15680d12cf972a9ad4b10b0601a315fb149102b
ilorez <zobirnajdaoui@gmail.com>
2024-10-26 09:33:57 +0100

    comment tests ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,22 +6,22 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 08:58:14 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/10/26 09:31:11 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/10/26 09:32:52 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "string.h"
-#include "stdio.h"
-#include "stdlib.h"
+//#include "string.h"
+//#include "stdio.h"
+//#include "stdlib.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char *us1;
-	unsigned char *us2;
-	size_t i;
+	unsigned char	*us1;
+	unsigned char	*us2;
+	size_t			i;
 
-	us1 = (unsigned char*)s1;
-	us2 = (unsigned char*)s2;
+	us1 = (unsigned char *)s1;
+	us2 = (unsigned char *)s2;
 	i = -1;
 	while (++i < n)
 		if (us1[i] != us2[i])
@@ -29,14 +29,15 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	return (0);
 }
 
-int main(int ac, char **av)
+/*int main(int ac, char **av)
 {
-	
+
 	char *str1;
 	str1 = av[1];
 	char *str2;
 	str2 = av[2];
-	int diff = (int)ft_memcmp((const void *)str1, (const void *)str2,atoi(av[3]));
+	int diff = (int)ft_memcmp((const void *)str1,
+			(const void *)str2,atoi(av[3]));
 	printf("str1:%s\nstr2:%s\ndeferent byte:%d\n", str1, str2, diff);
-	return ac;
-}
+	return (ac);
+}*/
