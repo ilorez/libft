83997204f38d8956b7419caadc63e6e0c1354821
ilorez <zobirnajdaoui@gmail.com>
2024-10-27 21:07:36 +0100

    strtrim we just start

---
+++ ft_strtrim.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 20:21:59 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/10/27 20:35:44 by znajdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// funciton for test if current caracter in set or not
+char *ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	start;
+	
+	while (s1[i])
+	{
+		while (s1[i] in set)
+	}
+	
+		
+}
