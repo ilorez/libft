2f0e3f4138efc7f8ccd4662ac082c3b0c092320d
ilorez <zobirnajdaoui@gmail.com>
2024-10-27 11:34:36 +0100

    strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,8 +6,50 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 10:02:38 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/10/27 10:10:41 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/10/27 11:27:49 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	len;
+
+	len = 0;
+	if (dstsize)
+	{
+		while (src[len] && --dstsize)
+		{
+			dst[len] = src[len];
+			len++;
+		}
+		dst[len] = '\0';
+	}
+	while (src[len])
+	{
+		len++;
+	}
+	return (len);
+}
+char *ft_strdup(const char *s1)
+{	
+	char	*dst;
+	size_t len;
+	
+	len = ft_strlen(s1) + 1;
+	dst = (char *)malloc(len);
+	if (!dst)
+		return (NULL);
+	return (ft_strlcpy(dst, s1, len));
+}
--- libft.h
+++ libft.h
@@ -6,8 +6,12 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 09:06:37 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/10/23 09:06:56 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/10/27 11:23:23 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stdlib.h"
+#ifndef LIBFT_H
+#define LIBFT_H
+	#include "stdlib.h"
+	
+#endif
