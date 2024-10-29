72f26aec599da4d19fe58df774e2375bd3ff9f14
ilorez <zobirnajdaoui@gmail.com>
2024-10-29 18:58:13 +0100

    chore: Refactor ft_split function to improve readability and performance

--- ft_split.c
+++ ft_split.c
@@ -6,21 +6,48 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 12:51:59 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/10/29 13:09:41 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/10/29 18:32:49 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+static int	_ft_c_ws(char *str, char *seps)
+{
+	int	i;
+	int	w;
+
+	w = 0;
+	i = 0;
+	while (str[i])
+	{
+		while (str[i] && ft_is_sep(str[i], seps))
+			i++;
+		if (str[i])
+			w++;
+		while (str[i] && !ft_is_sep(str[i], seps))
+			i++;
+	}
+	return (w);
+}
 
 char **ft_split(char const *s, char c)
 {
+  char	**strings;
+	char	*word;
+	char	*start;
+	int		i;
+
+	i = 0;
   const char *c_str[] = {c, '\0'} 
   char *str = ft_strtrim(s, c_str);
+	strings = (char **)malloc(sizeof(char *) * (_ft_c_ws(str, charset) + 1));
+  // go word by word and allocate a place for it if didn't allocated run free function
+
   // use split of pecine 
-  // get count words for split of pecine and set it as static function
   // you should frees so create function for freeying a array that stop on found null
 
   printf(str)
 }
+
 
 
--- test.c
+++ test.c
@@ -1,6 +1,6 @@
+#include "unistd.h"
 #include "stdio.h"
-
-int main()
+/*int main()
 {
 	int d = 10;
 	int *ptr1 = &d;
@@ -12,4 +12,20 @@ int main()
 	printf("ptr1:%p\nptr2:%p\nd:%d\n", ptr1, ptr2, d);
 	printf("&ptr1:%p\n&ptr2:%p\n&d:%p\n", &ptr1, &ptr2, &d);
 	printf("output:%p", *&*ptr2);
+}*/
+int main()
+{
+  //char c = 'a';
+  char str[] = "hllo";
+  *(str + 1) = 'a';
+  printf("%s\n", str);
+  // change c to string so have terminating null
+  //
+  
+  //char str[] = {c, '\0'};
+ /* while (*str != '\0') {
+    *str = 'a';
+    write(1, str, 1);
+    str++;
+}*/
 }
