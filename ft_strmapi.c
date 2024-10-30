a86331eddd96f389c1269958012afd0cfd342480
ilorez <zobirnajdaoui@gmail.com>
2024-10-30 15:50:54 +0100

    striteri and strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 14:58:19 by znajdaou          #+#    #+#             */
+/*   Updated: 2024/10/30 15:30:12 by znajdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	size_t	len;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	if (!f)
+		return (ft_strdup(s));
+	len = ft_strlen(s);
+	i = -1;
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	while (++i < len)
+		str[i] = f(i, s[i]);
+	return (str);
+}
+
+/*char _ft_toupper(unsigned int i, char c)
+{
+  if (i % 2)
+	return (c - 32);
+  return (c);
+}
+int	main(int ac, char **av)
+{
+  if (ac == 2)
+		printf("%s\n", ft_strmapi(av[1], &_ft_toupper));
+	printf("null function: %s\n", ft_strmapi(av[1], NULL));
+	printf("null string: %s\n", ft_strmapi(NULL, &_ft_toupper));
+  return (0);
+}*/
