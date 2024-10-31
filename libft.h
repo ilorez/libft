f0bbc36294371bbb3caf14ffec0174859d16669b
ilorez <zobirnajdaoui@gmail.com>
2024-10-31 11:42:40 +0100

    start in linked list with first two functions'

--- libft.h
+++ libft.h
@@ -17,6 +17,27 @@
 	#include <stdlib.h>
 	#include <stdio.h>
   #include <unistd.h>
+
+	// structure
+	typedef struct s_list
+	{
+		void	*content;
+		struct s_list	*next;
+	}	t_list;
+
+	// list functions
+	t_list *ft_lstnew(void *content);
+	void ft_lstadd_front(t_list **lst, t_list *new);
+	int ft_lstsize(t_list *lst);
+	t_list *ft_lstlast(t_list *lst);
+	void ft_lstadd_back(t_list **lst, t_list *new);
+	void ft_lstdelone(t_list *lst, void (*del)(void *));
+	void ft_lstclear(t_list **lst, void (*del)(void *));
+	void ft_lstiter(t_list *lst, void (*f)(void *));
+	t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+
+
 	
 	// memory functions
 	void	*ft_memset(void *b, int c, size_t len); // this function is for setting a memory block to a specific value
