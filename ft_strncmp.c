f27b43b10cfb19000bac92724ec3a3d199f761da
ilorez <zobirnajdaoui@gmail.com>
2024-11-12 15:47:30 +0100

    fix: Update ft_strncmp to handle edge case of n being 0

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 18:30:18 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/11/03 10:13:09 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/11/12 15:46:03 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
 	while ((s2[i] || s1[i]) && i < n)
 	{
@@ -25,7 +27,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return (0);
 }
+/*#include <stdio.h>
+#include <string.h>
 
+int	main(void)
+{
+  ft_strncmp(NULL, NULL, 0);
+}*/
 /*int main(int ac, char **av)
 {
 	printf("str1:%s\nstr2:%s\nretuned:%d\n", av[1], av[2], ft_strncmp(av[1],
