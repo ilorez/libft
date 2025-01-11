d23f975e91c694bf1a4d74dc015c3554da3bd257
ilorez <zobirnajdaoui@gmail.com>
2025-01-11 17:43:09 +0100

    some edits but no tests:

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 17:43:27 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/12/31 11:56:29 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/01/11 17:40:12 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -21,7 +21,7 @@
 	* @note: the funciton always return a new allocated string
   * or null that can always be freed
  **/
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2, char const *c)
 {
 	char	*dst;
 	size_t	i;
@@ -36,13 +36,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	}
 	if (!s1 && !s2)
 		return (ft_strdup(""));
-	size = ft_strlen(s1) + ft_strlen(s2) + 1;
+	size = ft_strlen(s1) + ft_strlen(s2) + ft_strlen(c) + 1;
 	dst = ft_calloc(sizeof(char), size);
 	if (!dst)
 		return (NULL);
 	i = 0;
 	while (*s1)
 		dst[i++] = *s1++;
+  while (c && *c)
+		dst[i++] = *c++;
 	while (*s2)
 		dst[i++] = *s2++;
 	dst[i] = '\0';
